b10490e6eed9c226c480f36829ff5b49b128e629
zhongyuhan <715181149@qq.com>
2019-05-10 09:32:02 +0800

    feat: 完整

--- hooks/hookTest.jsx
+++ hooks/hookTest.jsx
@@ -1,4 +1,4 @@
-import React,{ useState,useEffect,useContext,useReducer } from 'react';
+import React,{ useState,useEffect,useContext,useReducer,useCallback,useMemo,useRef,useImperativeHandle,useLayoutEffect,forwardRef,useImperativeMethods } from 'react';
 // import {localeContext} from './context'
 import { UserProvider, UserContext } from './UserContext' 
 
@@ -106,6 +106,8 @@ function Counter({initialCount}) {
 function Example(props) {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);  //钩子函数
+  let [name2, setName2] = useState("Nate")
+  let nameRef = useRef()
   const id=555
   const isOnline = useFriendStatus(id);
   const width =useWindowWidth()
@@ -113,6 +115,9 @@ function Example(props) {
   const surname = useFormInput('Poppins')
   useDocumentTitle(name.value+''+surname.value)
   // const locale = useContext(localeContext)
+  const submitButton = () => {
+    setName2(nameRef.current.value);
+  };
 
 
   // 类似于 componentDidMount 和 componentDidUpdate:
@@ -132,6 +137,10 @@ function Example(props) {
 
   return (
     <div>
+      <p>这是ref:{name2}</p>
+      <button type="button" onClick={submitButton}>
+          改变ref
+      </button>
       <p>You clicked {count} times</p>
       {/* {locale} */}
       <UserProvider>
@@ -140,7 +149,7 @@ function Example(props) {
 
        <br/>
       宽度：{width}
-      <input {...name}/>
+      <input {...name} ref={nameRef}/>
       <br/>
       <input {...surname}/>
       <br/>
@@ -155,13 +164,56 @@ function Example(props) {
   );
 }
 
+//第二个参数传入一个数组，数组中的每一项一旦值或者引用发生改变，useCallback 就会重新返回一个新的记忆函数提供给后面进行渲染。
+function App1() {
+  const memoizedHandleClick = useCallback(() => {
+    console.log('Click happened')
+  }, []); // 空数组代表无论什么情况下该函数都不会发生改变
+  // return <SomeComponent onClick={memoizedHandleClick}>Click Me</SomeComponent>;
+}
+
+function App2() {
+  const memoizedHandleClick = useMemo(() => () => {
+    console.log('Click happened')
+  }, []); // 空数组代表无论什么情况下该函数都不会发生改变
+  // return <SomeComponent onClick={memoizedHandleClick}>Click Me</SomeComponent>;
+}
+
+function ChildInputComponent(props, ref) {
+  const inputRef = useRef(null);
+  useImperativeHandle(ref, () => inputRef.current);
+  return <input type="text" name="child input" ref={inputRef} />;}
+  const ChildInput = forwardRef(ChildInputComponent);function App() {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+  return (
+    <div>
+      <ChildInput ref={inputRef} />
+    </div>
+  );}
+
+  function FancyInput(props, ref) {
+    const inputRef = useRef();
+    useImperativeMethods(ref, () => ({
+      focus: () => {
+        inputRef.current.focus();
+      }
+    }));
+    return <input ref={inputRef}  />;
+  }
+  FancyInput = forwardRef(FancyInput);
+
 //useContext可以订阅React context而不用引入嵌套  获取上下文的传的props
 //useReducer则允许你使用一个reducer来管理一个复杂组件的局部状态（local state)
 // const [todos, dispatch] = useReducer(todosReducer);
 //useCallback 记忆函数
-//useMemo 记忆组件
+//useMemo 记忆组件  可以替代useCallback
+//useCallback 不会执行第一个参数函数，而是将它返回给你，而 useMemo 会执行第一个函数并且将函数执行结果返回给你。
 //useRef 保存引用值
-//useImperativeHandle 透传 Ref
+//useImperativeHandle 透传 Ref  用于让父组件获取子组件内的索引
+//useImperativeMethods 同上
 //useLayoutEffect 同步执行副作用
 //useMutationEffect 但它在React执行其DOM突变的同一阶段同步触发，然后更新兄弟组件。使用它来执行自定义DOM突变。useEffect尽可能优先选择标准，以避免阻止视觉更新
 //缺点：但是当下 v16.8 的版本中，还无法实现 getSnapshotBeforeUpdate 和 componentDidCatch 这两个在类组件中的生命周期函数。
