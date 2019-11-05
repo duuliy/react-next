814a4b0eba72b797d4e24e80e1faeb1c740eb335
zhongyuhan <715181149@qq.com>
2019-11-05 17:43:28 +0800

    feat: 新增react-redux的connect核心context用法

--- 手动redux/src/App.js
+++ 手动redux/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ThemeContext, themes} from './theme-context';
+import ThemedButton from './components/test';
 
 import { createStore, combineReducers,bindActionCreators } from './redux';
 import counterReducer from './reducers/counter';
@@ -41,26 +43,66 @@ actions.increment(); /*自增*/
 actions.increment(); /*自增*/
 actions.setName('duuliy'); /*修改 info.name*/
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-function App() {
+// 一个使用 ThemedButton 的中间组件
+function Toolbar(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemedButton onClick={props.changeTheme}>
+      Change Theme
+    </ThemedButton>
   );
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: { color: 'rgb(0, 51, 254)'},
+    };
+
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme:
+          state.theme === themes.dark
+            ? themes.light
+            : themes.dark,
+      }));
+    };
+  }
+
+  render() {
+    // 在 ThemeProvider 内部的 ThemedButton 按钮组件使用 state 中的 theme 值，
+    // 而外部的组件使用默认的 theme 值
+    return (
+      <>
+        <ThemeContext.Provider value={this.state.theme}>
+          <Toolbar changeTheme={this.toggleTheme} />
+        </ThemeContext.Provider>
+        <>
+          {/* <ThemedButton /> */}
+        </>
+      </>
+    );
+  }
 }
 
 export default App;
