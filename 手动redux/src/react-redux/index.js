49a4d94aad445140ff5edb4f9ae86386ce347202
zhongyuhan <715181149@qq.com>
2019-11-05 16:37:59 +0800

    feat: 对比react-redux核心区别

---
+++ 手动redux/src/react-redux/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import storeShape from '../utils/storeShape';
+import shallowEqual from '../utils/shallowEqual';
+/**
+ * connect()的实现  核心代码
+ * mapStateToProps 缺省时，不关联state
+ * mapDispatchToProps 缺省时，设置其默认值为 dispatch => ({dispatch})，将`store.dispatch` 方法作为属性传递给组件
+ */ 
+const defaultMapStateToProps = state => ({});
+const defaultMapDispatchToProps = dispatch => ({ dispatch });
+
+function getDisplayName(WrappedComponent) {
+    return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
+export default function connect(mapStateToProps, mapDispatchToProps) {
+    if(!mapStateToProps) {
+        mapStateToProps = defaultMapStateToProps;
+    }
+    if(!mapDispatchToProps) {
+        //当 mapDispatchToProps 为 null/undefined/false...时，使用默认值
+        mapDispatchToProps = defaultMapDispatchToProps;
+    }
+    return function wrapWithConnect (WrappedComponent) {
+        return class Connect extends Component {
+            static contextTypes = storeShape;
+            static displayName = `Connect(${getDisplayName(WrappedComponent)})`;
+            constructor(props) {
+                super(props);
+                //源码中是将 store.getState() 给了 this.state
+                this.state = mapStateToProps(store.getState(), this.props);
+                if(typeof mapDispatchToProps === 'function') {
+                    this.mappedDispatch = mapDispatchToProps(store.dispatch, this.props);
+                }else{
+                    //传递了一个 actionCreator 对象过来
+                    this.mappedDispatch = bindActionCreators(mapDispatchToProps, store.dispatch);
+                }
+            }
+            componentDidMount() {
+                this.unsub = store.subscribe(() => {
+                    const mappedState = mapStateToProps(store.getState(), this.props);
+                    if(shallowEqual(this.state, mappedState)) {
+                        return;
+                    }
+                    this.setState(mappedState);
+                });
+            }
+            componentWillUnmount() {
+                this.unsub();
+            }
+            render() {
+                return (
+                    <WrappedComponent {...this.props} {...this.state} {...this.mappedDispatch} />
+                )
+            }
+        }
+    }
+}
