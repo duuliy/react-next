1aa8c417fc04e2210941920cb6dde995063328b7
duuliy <715181149@qq.com>
2020-09-05 21:26:41 +0800

    refactor: 移动位置

--- duuliy-ui2/components/alert/demo/1-demo-basic.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Alert from 'duuliy-ui/lib/alert';
-import 'duuliy-ui/lib/alert/style';
-
-export default () => <Alert kind="warning">这是一条警告提示</Alert>;
--- duuliy-ui2/gatsby-config.js
+++
@@ -1,4 +0,0 @@
-//生成静态网页
-module.exports = {
-  plugins: ['gatsby-theme-docz', 'gatsby-plugin-less', 'gatsby-plugin-styled-components'],
-};
