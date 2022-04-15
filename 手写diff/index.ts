348cf8ec1edcd4d8d492f899ec4282fb80a71b5b
dan <duuliy667@gmail.com>
2022-04-15 11:27:12 +0800

    feat: diff test2

--- 手写diff/index.ts
+++ 手写diff/index.ts
@@ -5,3 +5,13 @@ const before = [{ key: "a" }, { key: "b" }, { key: "c" }, { key: "d" }];
 const after = [{ key: "d" }, { key: "a" }, { key: "b" }, { key: "c" }];
 
 console.log(diff(before, after));
+
+// // 更新前
+// const before = [
+//   {key: 'a'}
+// ]
+// // 更新后
+// const after = [
+//   {key: 'd'}
+// ]
+
